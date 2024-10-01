ccca50904b8e977c7511f4e6e92e8fb9eb7f2739
Fire Chicken <firechickenproductivity@gmail.com>
2024-09-30 19:46:17 -0600

    Add logger utility class

---
+++ source/logging_utilities.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+import file_utilities
+
+class Logger:
+    """A logger is used to record logging messages to a log file."""
+    def __init__(self, path):
+        """
+        path: the path to the log file.
+        A file will be created at the path if one does not exist.
+        """
+        self.path = path
+        file_utilities.create_file_at_path_if_nonexistent(path)
+    
+    def log_message(self, message: str):
+        """
+        message: the message to store in the log.
+        The current timestamp is pretended to the message.
+        """
+        with open(self.path, "a+") as file:
+            timestamp = datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
+            file.write(f"{timestamp}: {message}\n")
