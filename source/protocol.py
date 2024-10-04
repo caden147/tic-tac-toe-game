a2d90126ed8ee2bb8ca776fa7cba6716b4d93871
Fire Chicken <firechickenproductivity@gmail.com>
2024-10-03 21:42:09 -0600

    Add size fields

--- source/protocol.py
+++ source/protocol.py
@@ -8,16 +8,24 @@ class ProtocolField:
         """Gives the text used to represent the field in a struct.pack or struck.unpack call"""
         pass
 
+    def get_size(self):
+        """Returns the size in bytes of the field"""
+        pass
+
 class SimpleProtocolField(ProtocolField):
-    def __init__(self, name: str, struct_text: str):
+    def __init__(self, name: str, struct_text: str, size: int):
         self.name = name
         self.struct_text = struct_text
+        self.size = size
 
     def get_name(self):
         return self.name
     
     def compute_struct_text(self):
         return self.struct_text
+    
+    def get_size(self):
+        return self.size
 
 class MessageProtocol:
     def __init__(self, type_code, fields):
@@ -41,4 +49,13 @@ class MessageProtocol:
             results[name] = values[i]
         return results
 
+    def get_type_code(self):
+        return self.type_code
+    
+    def compute_size(self):
+        """Returns the size in bytes of a message using the protocol"""
+        size = 0
+        for i in range(len(values)):
+            size += self.fields[i].get_size()
+        return size
 
