038a68d2e4412b5e57012a57c1e751e68f07e5c7
Fire Chicken <firechickenproductivity@gmail.com>
2024-10-06 16:17:56 -0600

    Make last argument optional

--- source/client.py
+++ source/client.py
@@ -33,12 +33,15 @@ def start_connection(host, port, type_code, request):
     sel.register(sock, events, data=message)
 
 
-if len(sys.argv) != 5:
-    print("usage:", sys.argv[0], "<host> <port> <action> <value>")
+if len(sys.argv) not in [4, 5]:
+    print("usage:", sys.argv[0], "<host> <port> <action> [<value>]")
     sys.exit(1)
 
 host, port = sys.argv[1], int(sys.argv[2])
-action, value = sys.argv[3], sys.argv[4]
+action = sys.argv[3]
+value = ""
+if len(sys.argv) == 5:
+    value = sys.argv[4]
 type_code, request = create_request(action, value)
 start_connection(host, port, type_code, request)
 
