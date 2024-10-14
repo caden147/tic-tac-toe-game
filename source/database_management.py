0f54d0e44c0280382b35767b157d0286a629758e
Fire Chicken <firechickenproductivity@gmail.com>
2024-10-14 15:29:39 -0600

    Improve testing and documentation

--- source/database_management.py
+++ source/database_management.py
@@ -6,13 +6,24 @@ import sqlite3
 #Classes for representing database tables
 
 class TableField:
+    """Represents one of the values in a table entry"""
     def __init__(self, name: str, data_type: str, is_primary_key: bool = False):
+        """
+            name: the name of the field
+            data_type: the data type of the field as understood by the database
+            is_primary_key: determines if this is the primary key for the table
+        """
         self.name = name
         self.data_type = data_type
         self.is_primary_key = is_primary_key
-    
+
 class Table:
+    """Represents a database table consisting of rows with specific fields"""
     def __init__(self, name: str, fields):
+        """
+            name: the name of the table
+            fields: and ordered iterable of the fields every entry in this table must have
+        """
         self.name = name
         self.fields = fields
         self.primary_key = None
@@ -23,13 +34,18 @@ class Table:
 
 # Database entry representation classes
 class Account:
+    """Represents a user account"""
     def __init__(self, name, password):
+        """
+            name: the user name associated with the account
+            password: the user's password
+        """
         self.name = name
         self.password = password
 
-#Database management helper functions. Other modules should not call these
+#Database management helper functions. Other modules should not call these.
 
-def create_table_if_nonexistent_using_cursor(table: Table, cursor):
+def _create_table_if_nonexistent_using_cursor(table: Table, cursor):
     """
         Creates the specified database table using a cursor object if the table does not already exist
         table: A database table object
@@ -47,7 +63,11 @@ def create_table_if_nonexistent_using_cursor(table: Table, cursor):
     creation_text += ")"
     cursor.execute(creation_text)
 
-def create_placeholders_for_fields(fields):
+def _create_placeholders_for_fields(fields):
+    """
+        Creates the placeholder text for the specified fields
+        fields: an iterable of TableField objects
+    """
     placeholder = "("
     added_placeholder_before = False
     for _ in fields:
@@ -58,7 +78,7 @@ def create_placeholders_for_fields(fields):
     placeholder += ")"
     return placeholder
 
-def insert_values_into_table_for_database_at_path(values, table: Table, path: str):
+def _insert_values_into_table_for_database_at_path(values, table: Table, path: str):
     """
         Inserts the values into the specified table for the database at the path.
         This can throw a sqlite3.Error exception!
@@ -67,7 +87,7 @@ def insert_values_into_table_for_database_at_path(values, table: Table, path: st
         path: the path containing the database
     """
     connection = sqlite3.connect(path)
-    insertion_command = f"INSERT INTO {table.name} VALUES" + create_placeholders_for_fields(table.fields)
+    insertion_command = f"INSERT INTO {table.name} VALUES" + _create_placeholders_for_fields(table.fields)
     exception = None
     try:
         with connection:
@@ -78,7 +98,13 @@ def insert_values_into_table_for_database_at_path(values, table: Table, path: st
     if exception:
         raise exception
 
-def retrieve_values_from_table_from_database_at_path_using_primary_key(table: Table, path: str, primarykey):
+def _retrieve_values_from_table_from_database_at_path_using_primary_key(table: Table, path: str, primarykey):
+    """
+        Returns values from a table at the specified database path associated with the primary key if present or otherwise returns None
+        table: the table in the database to retrieve values from
+        path: the path to the database
+        primarykey: the primary key value used to identify the target row in the database
+    """
     connection = sqlite3.connect(path)
     retrieval_command = f"SELECT * FROM {table.name} WHERE {table.primary_key.name} = ?"
     cursor = connection.cursor()
@@ -93,13 +119,21 @@ TABLES = [ACCOUNT_TABLE]
 #The public interface: functions intended to be used by other modules
 
 def insert_account_into_database_at_path(account: Account, path: str):
+    """
+        Inserts the account object into the database at the specified path.
+        Raises an sqlite3.Error exception if the account already exists.
+        account: an Account object
+        path: the path to the database
+    """
     values = (account.name, account.password)
-    insert_values_into_table_for_database_at_path(values, ACCOUNT_TABLE, path)
+    _insert_values_into_table_for_database_at_path(values, ACCOUNT_TABLE, path)
 
 def retrieve_account_with_name_from_database_at_path(name: str, path: str):
-    values = retrieve_values_from_table_from_database_at_path_using_primary_key(ACCOUNT_TABLE, path, name)
-    account = Account(*values)
-    return account
+    values = _retrieve_values_from_table_from_database_at_path_using_primary_key(ACCOUNT_TABLE, path, name)
+    result = None
+    if values:
+        result = Account(*values)
+    return result
 
 def create_database_at_path(path: str):
     """
@@ -109,7 +143,7 @@ def create_database_at_path(path: str):
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
     for table in TABLES:
-        create_table_if_nonexistent_using_cursor(table, cursor)
+        _create_table_if_nonexistent_using_cursor(table, cursor)
     connection.commit()
     connection.close()
 
@@ -123,3 +157,5 @@ if __name__ == '__main__':
     print('result', result)
     if result is not None:
         print(result.name, result.password)
+    empty_result = retrieve_account_with_name_from_database_at_path('george', database_path)
+    print('empty_result', empty_result)
