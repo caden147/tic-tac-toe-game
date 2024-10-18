ebbfc66ea454b7638a647f51cf4b3b5e52e8df9a
Fire Chicken <firechickenproductivity@gmail.com>
2024-10-18 12:30:10 -0600

    Improve organization

--- source/connection_handler.py
+++ source/connection_handler.py
@@ -98,13 +98,21 @@ class MessageReceiver:
     
     def process_complete_message(self):
         """Finishes handling a completed message"""
+        #Extract the information from the message handler
         values = self.message_handler.get_values()
         type_code = self.message_handler.get_protocol_type_code()
         content_length = self.message_handler.get_number_of_bytes_extracted()
+        
+        #Have the message handler prepare for receiving the next message
         self.message_handler.prepare_for_next_message()
+
+        #Add the message to the queue to be processed
         message = Message(type_code, values)
         self.messages.append(message)
+
         print("received message with type code", type_code, repr(message), "from", self.addr)
+
+        #Start processing whatever is remaining in the buffer
         if len(self.buffer) > 0:
             #If there is anything still in the buffer, process the new request
             #This is necessary because the selector will only call read when bytes are received, which will cause issues if multiple messages arrive simultaneously
@@ -133,17 +141,20 @@ class ConnectionHandler:
         self.selector = selector
         self.connection_information = connection_information
         self.is_server = is_server
+
+        #Pick the correct protocol maps based on of this is the client or the server
         if self.is_server:
             sending_protocol_map = protocol_definitions.CLIENT_PROTOCOL_MAP
             receiving_protocol_map = protocol_definitions.SERVER_PROTOCOL_MAP
         else:
             sending_protocol_map = protocol_definitions.SERVER_PROTOCOL_MAP
             receiving_protocol_map = protocol_definitions.CLIENT_PROTOCOL_MAP
+            
         self.logger = logger
-        self.message_sender = MessageSender(self.logger, self.connection_information, sending_protocol_map, self.close)
         self.callback_handler = callback_handler
         message_handler = protocol.MessageHandler(receiving_protocol_map)
         self.message_receiver = MessageReceiver(self.logger, connection_information, message_handler, self.close)
+        self.message_sender = MessageSender(self.logger, self.connection_information, sending_protocol_map, self.close)
 
     def _set_selector_events_mask(self, mode):
         """Set selector to listen for events: mode is 'r', 'w', or 'rw'."""
