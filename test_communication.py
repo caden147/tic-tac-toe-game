dd2d236abad85e013d2fa70299df19d461e4751b
Fire Chicken <firechickenproductivity@gmail.com>
2024-10-26 22:07:27 -0600

    Enable server closing during multithreaded testing

--- test_communication.py
+++ test_communication.py
@@ -20,8 +20,12 @@ class TestMocking(unittest.TestCase):
         client = Client('200', 19, client_selector, client_logger, output_text_function=lambda x: client_output.append(x), socket_creation_function=internet.create_socket_from_address)
         server = Server('localhost', 9090, server_selector, server_logger, 'testing.db', internet.create_listening_socket_from_address)
         server_listening_thread = Thread(target=server.listen_for_socket_events)
-        server_listening_thread.start()
-        client.send_message(Message(protocol_definitions.BASE_HELP_MESSAGE_PROTOCOL_TYPE_CODE, []))
+        try:
+            server_listening_thread.start()
+            client.send_message(Message(protocol_definitions.BASE_HELP_MESSAGE_PROTOCOL_TYPE_CODE, []))
+        except Exception as exception:
+            server.close()
+            raise exception
 
 if __name__ == '__main__':
     unittest.main()
