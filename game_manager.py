d5473ed12d4c55a21c12b0734b11856d75f837c7
Fire Chicken <firechickenproductivity@gmail.com>
2024-11-02 19:05:01 -0600

    Draft server side support

--- game_manager.py
+++ game_manager.py
@@ -9,6 +9,9 @@ class Game:
         if self.current_turn not in self.players:
             raise ValueError("Invalid current turn")
 
+    def compute_player_piece(self, username: str):
+        return 'X' if username == self.creator_username else 'O'
+
     def make_move(self, username, move):
         if username != self.current_turn:
             return False
