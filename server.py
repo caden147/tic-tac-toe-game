7dfbc5bc8c928509b8501261d2d9dc603642e372
Fire Chicken <firechickenproductivity@gmail.com>
2024-11-02 19:12:39 -0600

    Use username

--- server.py
+++ server.py
@@ -136,7 +136,7 @@ class Server:
             if joiner_state.current_game is not None:
                 self.handle_game_quit({}, connection_information)
             joiner_state.current_game = game
-            player_piece = game.compute_player_piece()
+            player_piece = game.compute_player_piece(joiner_username)
             piece_message = Message(protocol_definitions.GAME_PIECE_PROTOCOL_TYPE_CODE, (player_piece,))
             self.connection_table.send_message_to_entry(piece_message, connection_information)
             game_text = game.compute_text()
